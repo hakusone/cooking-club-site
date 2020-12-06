75b0da6a8fbf9e6c6e2209baf3949327f19d9e76
hakusone <vredesyn@gmail.com>
2020-12-06 13:58:35 -0800

    final initial commit

--- Practice/fileread.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let fname = __dirname + '/index.html';
-let fdata = fs.readFileSync(fname, 'utf-8');
-let flines = fdata.split('\n');
-console.log('Lines in file index.html: ' + flines.length);
